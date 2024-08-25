1bf18b465dd6a9638745239b430a97ebdbd55aec
Aryan Jain <aryanofficialjain@gmail.com>
2024-08-25 08:53:01 +0530

    completely tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const [notifications, setNotifications] = useState([]); // State for notifications
 
   useEffect(() => {
-    const socketInstance = io("http://localhost:3000");
+    const socketInstance = io(import.meta.env.VITE_SITE_URL);
     setSocket(socketInstance);
 
     socketInstance.on("connect", () => {
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const server = createServer(app);
 
 const storage = multer.diskStorage({
